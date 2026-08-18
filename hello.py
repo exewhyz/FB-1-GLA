1f4f3c9b370be835e629e67a480d144ae2fa9b1b
Aniket Raj <araj52444@gmail.com>
2026-08-18 16:35:53 +0530

    Add initial version of hello.py with comments and function definition

---
+++ hello.py
@@ -0,0 +1,27 @@
+"""We are learning comments.
+And also types of comment
+this is a multi line comment"""
+
+'''
+this is also multi line comment
+using triple single quote
+'''
+
+
+# this is a single line comment
+print("this is our first program") # this is inline comment
+
+
+# print(print.__doc__)
+# print(help(print))
+
+#adding two numbers
+def add():
+    '''
+    this is a docstring
+    using triple single quote
+    '''
+    print(1+2)
+
+# print(add.__doc__)
+print(help(add))
