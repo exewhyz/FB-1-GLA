b24ee740e8e5d3657b8f5bc9bb71b2b0a4ce87f5
Aniket Raj <araj52444@gmail.com>
2026-08-19 16:46:23 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,55 @@
+full_name = "Aniket"
+age = 5
+is_student = False
+
+"""
+Data types
+1. string -> str
+2. integer -> int
+3. decimal -> float
+4. complex numbers -> complex
+5. boolean -> bool
+6. none -> None
+"""
+
+# string
+# double quote
+city = "Noida"
+#single quote
+country = 'India'
+# multi line string
+full_address="""hgvdjfh
+vfjdkl
+dbv
+"""
+
+#numbers
+
+#interger
+year = 2026
+#decimal
+price = 99.0
+#complex number
+c = 4 + (-5j)
+
+is_admin = True
+is_admin = False
+
+nothing = None
+
+type_of_data = type(is_admin)
+
+print(type_of_data)
+
+# isinstance(nothing, None)
+# print(isinstance(is_admin, int))
+
+num1 = "1"
+num2 = "2"
+# print(num1 + num2)
+
+# print(int(num1) + int(num2))
+
+int(3.57) # 3
+
+print(bool(" "))
